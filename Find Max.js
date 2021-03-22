b893d831158498ac21c2580f7258fa24bebc1178
egrilli <34517334+egrilli@users.noreply.github.com>
2021-03-22 14:01:48 -0300

    Update Find Max.js

--- Find Max.js
+++ Find Max.js
@@ -1,6 +1,6 @@
-Given an array with multiple values, write a function that returns the maximum number in the array. (e.g. for [-3,3,5,7] max is 7)
+/*Given an array with multiple values, write a function that returns the maximum number in the array. (e.g. for [-3,3,5,7] max is 7)
 Dada una matriz con varios valores, escriba una función que devuelva el número máximo en la matriz. (p. ej., para [-3,3,5,7] max es 7)
-
+*/
 
 
 function findMax(arr) {
