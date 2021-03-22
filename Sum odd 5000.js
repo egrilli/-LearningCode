9fadbdbb388544ae5ac8ba1f8eb5a29a5ef6b3fd
egrilli <34517334+egrilli@users.noreply.github.com>
2021-03-22 14:04:58 -0300

    Update Sum odd 5000.js

--- Sum odd 5000.js
+++ Sum odd 5000.js
@@ -1,8 +1,8 @@
-Write a function that returns the sum of all the odd numbers from 1 to 5000. (e.g. 1+3+5+...+4997+4999)
-Escribe una función que devuelva la suma de todos los números impares del 1 al 5000 (por ejemplo, 1 + 3 + 5 + ... + 4997 + 4999).
+//Write a function that returns the sum of all the odd numbers from 1 to 5000. (e.g. 1+3+5+...+4997+4999)
+//Escribe una función que devuelva la suma de todos los números impares del 1 al 5000 (por ejemplo, 1 + 3 + 5 + ... + 4997 + 4999).
 
 
-Option 1
+//Option 1
 
 function sum_odd_5000() {
     
@@ -19,7 +19,7 @@ function sum_odd_5000() {
 }
 
 
-Option 2
+//Option 2
 
 function sum_odd_5000() {
     
