1ebcf7bf75608f0402d609600e6f427d40ef0af8
egrilli <34517334+egrilli@users.noreply.github.com>
2021-03-23 13:22:32 -0300

    Create Swap-values.js

---
+++ Swap-values.js
@@ -0,0 +1,4 @@
+// Write a function that will swap the first and last values of any given array. The default minimum length of the array is 2. (e.g. [1,5,10,-2] will become [-2,5,10,1]).
+// Escribe una función que intercambie el primer y último valor de cualquier matriz dada. La longitud mínima predeterminada de la matriz es 2. (por ejemplo, [1,5,10, -2] se convertirá en [-2,5,10,1]).
+
+iii
